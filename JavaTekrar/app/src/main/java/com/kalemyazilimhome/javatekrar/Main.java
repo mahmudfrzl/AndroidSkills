7f2e63f11f2bfcfe6076aa162107c9aeb265ae88
Mahmud <mahmudfrzl@mail.ru>
2021-07-21 08:18:50 +0400

    My Android Java Skills

---
+++ JavaTekrar/app/src/main/java/com/kalemyazilimhome/javatekrar/Main.java
@@ -0,0 +1,5 @@
+package com.kalemyazilimhome.javatekrar;
+
+public class Main {
+    
+}
---
+++ MethodsAndClasses/app/src/main/java/com/kalemyazilimhome/methodsandclasses/MainActivity.java
@@ -0,0 +1,43 @@
+package com.kalemyazilimhome.methodsandclasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("on create called");
+        testMethod();
+        System.out.println(math(4,3));
+    }
+
+    public void testMethod(){
+        int x = 4+4;
+        System.out.println("Value of x: " + x);
+    }
+    public int math(int a, int b){
+        return a*b;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("on resume called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("on stop called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("on pause called");
+    }
+}
